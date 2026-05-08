9da63254c26e9aa92e35664057b54df02b157081
Xtimate <lordquestionmark.discord@gmail.com>
2026-05-08 22:24:45 +0200

    fixed merge

--- backend/model.py
+++ backend/model.py
@@ -1,2 +1,21 @@
 import torch
-from torch import nn.Module
+from torch import nn
+
+
+class FoodCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(FoodCNN, self).__init__()
+        self.block1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout2d(0.25),
+        )
+
+    def forward(self, x):
+        x = self.block1(x)
+        return x
