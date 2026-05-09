47437cec20db07dd8e9058e8b8ab122874fceb53
kittendevv <codingkitten@disroot.org>
2026-05-09 20:43:02 +0200

    Add some pages and stuff

--- frontend/src/lib/components/dock.svelte
+++ frontend/src/lib/components/dock.svelte
@@ -1,24 +1,25 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
+	import { page } from '$app/stores';
 
 	import MagnifyingGlassIcon from 'phosphor-svelte/lib/MagnifyingGlassIcon';
 	import ClockIcon from 'phosphor-svelte/lib/ClockIcon';
 	import GearIcon from 'phosphor-svelte/lib/GearIcon';
 </script>
 
-<div class="dock">
-	<a href={resolve('/search')} class="dock-active">
-		<MagnifyingGlassIcon weight="bold" />
+<div class="dock dock-xl">
+	<a href={resolve('/search')} class:dock-active={$page.url.pathname === '/search'}>
+		<MagnifyingGlassIcon weight="duotone" size={18} />
 		<span class="dock-label">Search</span>
 	</a>
 
 	<a href={resolve('/')}>
-		<ClockIcon weight="bold" />
+		<ClockIcon weight="duotone" size={18} />
 		<span class="dock-label">History</span>
 	</a>
 
 	<a href={resolve('/')}>
-		<GearIcon />
+		<GearIcon weight="duotone" size={18} />
 		<span class="dock-label">Settings</span>
 	</a>
 </div>
