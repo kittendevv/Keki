cb0aff10570505efef3f6f99164f0ec230d55835
Xtimate <lordquestionmark.discord@gmail.com>
2026-05-10 15:47:41 +0200

    fixed image tensor resizing

--- backend/api/main.py
+++ backend/api/main.py
@@ -141,7 +141,7 @@ with open(
 
 transform = T.Compose(
     [
-        T.Resize((224, 224)),
+        T.Resize((128, 128)),
         T.ToTensor(),
         T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
